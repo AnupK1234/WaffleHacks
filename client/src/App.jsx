260c4f97a6e269328259dc19e7d2a2c81b215d0b
Vidip Ghosh <vidip2001@gmail.com>
2024-06-23 15:28:02 +0530

    chore: resolves conflicts

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -11,15 +11,11 @@ import { AuthProvider } from "./context/AuthContext";
 function App() {
   return (
     <>
-      <Routes>
-      <Route path="/" element={<Home/>} />
-      <Route path="/donate" element={<Donate/>} />
-      <Route path="/dashboard" element={<Dashboard/>} />
-      </Routes>
       <AuthProvider>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/donate" element={<Donate />} />
+          <Route path="/donate" element={<Donate/>} />
+          <Route path="/dashboard" element={<Dashboard/>} />
           <Route path="/signup" element={<SignUp />} />
           <Route path="/signin" element={<SignIn />} />
           <Route path="/contactus" element={<Contact />} />
